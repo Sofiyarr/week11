edb1840dd2a6de661cc22d9d72a7d41b9a904c04
sofiya <enikeevasofiya@gmail.com>
2023-05-21 19:13:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function sale(){
+    let sum = document.getElementById ('sum');
+    console.log(sum.textContent);
+    sum.textContent = "1600p"
+}
+
+function priceA(){
+    let priceA = document.getElementById ('priceA');
+    console.log(priceA.textContent);
+    priceA.textContent = "400p"
+}
+
+function priceB(){
+    let priceB = document.getElementById ('priceB');
+    console.log(priceB.textContent);
+    priceB.textContent = "240p"
+}
+
+function priceC(){
+    let priceC = document.getElementById ('priceC');
+    console.log(priceC.textContent);
+    priceC.textContent = "640p"
+}
+
+function priceD(){
+    let priceD = document.getElementById ('priceD');
+    console.log(priceD.textContent);
+    priceD.textContent = "320p"
+}
